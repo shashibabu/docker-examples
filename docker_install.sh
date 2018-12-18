deeaff2fb886bc5e3fa5f54e487408878bcb696b
shashibabu <38233930+shashibabu@users.noreply.github.com>
2018-12-18 06:49:42 +0530

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get remove docker docker-engine docker.io -y
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y\
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get update
+sudo apt-get install docker-ce -y
